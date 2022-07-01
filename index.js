f31fd2de533ed8e01d6d6bd6e0d43b73c16e905f
zengchaowu <root@zengchaowu.com>
2022-07-01 11:49:07 +0800

    chore: remove bin

--- index.js
+++ index.js
@@ -4,21 +4,4 @@ import mkdirp from "@doraemon-module/nuxt-functions/lib/mkdirp";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-export default async () => {
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-
-  const network = mkdirp(path.join(process.cwd(), "network"));
-  const xmpp = mkdirp(path.join(network, "xmpp"));
-  copy(
-    path.join(__dirname, "lib", "connect.js"),
-    path.join(xmpp, "connect.js")
-  );
-  copy(
-    path.join(__dirname, "lib", "disconnect.js"),
-    path.join(xmpp, "disconnect.js")
-  );
-  copy(
-    path.join(__dirname, "lib", "createConnection.js"),
-    path.join(xmpp, "createConnection.js")
-  );
-};
+export default async () => {};
